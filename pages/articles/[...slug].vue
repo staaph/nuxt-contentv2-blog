ad53ea8445e85e162ae5e6f3e7adfcaa4a62c113
staaph <contact.staaph@gmail.com>
2022-06-15 22:39:13 +0200

    initial commit

---
+++ pages/articles/[...slug].vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const {
+  params: { slug },
+} = useRoute();
+
+const { data: article } = await useAsyncData(`article-${slug}`, () =>
+  queryContent(`/articles/${slug}`).findOne()
+);
+</script>
+
+<template>
+  <div class="">
+    <p>{{ article.title }}</p>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const { data: articles } = await useAsyncData('articles', () =>
+  queryContent('/articles').find()
+);
+</script>
+
+<template>
+  <div class="">
+    <div
+      class="flex flex-col items-center"
+      v-for="(article, idx) in articles"
+      :key="idx"
+    >
+      <NuxtLink :to="article._path">{{ article.title }}</NuxtLink>
+    </div>
+  </div>
+</template>
