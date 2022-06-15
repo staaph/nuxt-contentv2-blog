fee53bfa04b4c53acfcc37bc98b2d3b2ab3d86c5
staaph <contact.staaph@gmail.com>
2022-06-15 23:44:30 +0200

    added styles

--- pages/index.vue
+++ pages/index.vue
@@ -5,9 +5,28 @@ const { data: articles } = await useAsyncData('articles', () =>
 </script>
 
 <template>
-  <div>
-    <div v-for="article in articles" :key="article._id">
+  <main class="main">
+    <h1 class="title">Discover fascinating stories</h1>
+    <div v-for="article in articles" :key="article._id" class="grid">
       <NuxtLink :to="article._path">{{ article.title }}</NuxtLink>
     </div>
-  </div>
+  </main>
 </template>
+
+<style scoped>
+.main {
+  display: flex;
+  flex-direction: column;
+  max-width: 100vw;
+  padding: 0;
+  margin: 0;
+}
+
+.title {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: 700;
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
