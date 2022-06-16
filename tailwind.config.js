f125b879caae5ab82cc7305a7e59e29c04a080d8
staaph <contact.staaph@gmail.com>
2022-06-16 23:08:56 +0200

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,8 @@
-import config from './config';
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [],
   theme: {
     extend: {
-      colors: {
-        dark: config.bg_dark,
-        light: config.bg_light,
-      }
     },
   },
   darkMode: 'class',
