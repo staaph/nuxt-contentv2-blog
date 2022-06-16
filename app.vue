65133c5bf44a0baa7069746ba676bc3c092ed6ab
staaph <contact.staaph@gmail.com>
2022-06-16 18:05:35 +0200

    add meta

--- app.vue
+++ app.vue
@@ -3,3 +3,14 @@
     <NuxtPage />
   </div>
 </template>
+
+<script setup lang="ts">
+useHead({
+  title: 'My App',
+  // or, instead:
+  // titleTemplate: (title) => `My App - ${title}`,
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+  charset: 'utf-8',
+  meta: [{ name: 'description', content: 'Nuxt content v2 template' }],
+});
+</script>
