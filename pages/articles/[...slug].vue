c82ff6a7d565eeb99a4a823c74066b59067051cb
staaph <contact.staaph@gmail.com>
2022-06-29 14:06:00 +0200

    implement error handling

--- pages/articles/[...slug].vue
+++ pages/articles/[...slug].vue
@@ -11,10 +11,15 @@ const { data: allArticles } = await useAsyncData('article', () =>
   queryContent('/articles/').find()
 )
 
-useHead({
-  title: article.value.title,
-  meta: [{ name: 'description', content: `${article.value.description}` }]
+onBeforeMount(() => {
+  if (article.value) {
+    useHead({
+      title: article.value.title,
+      meta: [{ name: 'description', content: `${article.value.description}` }]
+    })
+  }
 })
+
 
 const articles = () => {
   const arr = []
@@ -34,35 +39,37 @@ const getCurrentIndex = () => {
     }
   }
 }
-
-const currentItem = getCurrentIndex().index
-const lastItem = articles()[articles().length - 1].index
 </script>
 
 <template>
-  <main>
-    <div class="content text-black dark:text-white leading-[2rem]">
-      <section class="flex flex-row justify-between">
-        <NuxtLink
-          v-if="currentItem != 0"
-          :to="`/articles/${articles()[currentItem - 1].articlePath}`"
-        >
-          <p class="arrow">
-            <IconsArrowLeft /> {{ articles()[currentItem - 1].articlePath }}
-          </p>
-        </NuxtLink>
-        <h1 class="title" v-text="article.title" />
-        <NuxtLink
-          v-if="currentItem != lastItem"
-          :to="`/articles/${articles()[currentItem + 1].articlePath}`"
-        >
-          <p class="arrow">
-            {{ articles()[currentItem + 1].articlePath }} <IconsArrowRight />
-          </p>
-        </NuxtLink>
-      </section>
-      <ContentRenderer :value="article" class="main" />
-    </div>
+  <main class="content text-black dark:text-white leading-[2rem]">
+    <section v-if="article">
+      <div>
+        <section class="flex flex-row justify-between">
+          <NuxtLink
+            v-if="getCurrentIndex().index != 0"
+            :to="`/articles/${articles()[getCurrentIndex().index - 1].articlePath}`"
+          >
+            <p class="arrow">
+              <IconsArrowLeft /> {{ articles()[getCurrentIndex().index - 1].articlePath }}
+            </p>
+          </NuxtLink>
+          <h1 class="title" v-text="article.title" />
+          <NuxtLink
+            v-if="getCurrentIndex().index != articles()[articles().length - 1].index"
+            :to="`/articles/${articles()[getCurrentIndex().index + 1].articlePath}`"
+          >
+            <p class="arrow">
+              {{ articles()[getCurrentIndex().index + 1].articlePath }} <IconsArrowRight />
+            </p>
+          </NuxtLink>
+        </section>
+        <ContentRenderer :value="article" class="main" />
+      </div>
+    </section>
+    <section v-else>
+      <Error />
+    </section>
   </main>
 </template>
 
